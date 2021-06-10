f5dacf7820df0f932530e87a5b9f0b352c8050f9
Notebook do mendes <marcelomendes@ideyas.it>
2021-06-10 08:58:29 -0300

    pegando dados de cidaes em um json e fazendo as requisições mostrando a temp previsão do tempo na tela

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from decouple import config
 import requests
-
+import json
 DEBUG=True
 if DEBUG:
-    key=f"key={config('KEY')}"
-    format="format=json"
-    locale="locale=pt"
-    cidade="woeid=455922"#id cidade de  vitória
-    url = f"https://api.hgbrasil.com/weather?{key}&{cidade}&{format}&{locale}/"
-    r = requests.get(url)
-    print(r.text)
+    key = f"key={config('KEY')}"
+    format = "format=json"
+    locale = "locale=pt"
+    json_file = open("cidades.json","r").read()
+    json_file = json.loads(json_file)
+    for value in json_file:
+        id = value["id"]
+        cidade=f"woeid={id}"
+        print(cidade)
+        url = f"https://api.hgbrasil.com/weather?{key}&{cidade}&{format}&{locale}/"
+        r = requests.get(url)
+        response = json.loads(r.text)
+        response = response['results']
+        print(response)
+        input("\n aperte enter para ver a proxima cidade.")
